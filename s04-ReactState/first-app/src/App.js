52db718456f858a94cf56c3ac6646a6743b30e3d
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2021-10-18 12:34:28 +0200

    section 03 - react basic

---
+++ s04-ReactState/first-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Expenses } from "./components/Expenses";
+import { expenses } from "./data/expenses";
+
+export const App = () => {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses expenses={expenses} />
+    </div>
+  );
+};
+
+export default App;
